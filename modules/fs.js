0ff3b77786f45a50abe16c042da4610f3dd6e749
Lucas Simidu <lucas.simidu@outlook.com>
2025-08-21 02:02:14 -0300

    feat: fs module examples

---
+++ modules/fs.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+const path = require('path');
+
+//Criar uma pasta
+fs.mkdir(path.join(__dirname, '/test'), (error) => {
+    if (error) {
+        return console.log("Erro: " + error);
+    }
+
+    console.log("Pasta criada com sucesso");
+})
+
+// Criar um arquivo
+fs.writeFile(path.join(__dirname, '/test', 'test.txt'), 'hello world', (error) => {
+    if (error) {
+        return console.log("Erro: " + error);
+    }
+
+    console.log("Arquivo criado com sucesso");
+    
+})
