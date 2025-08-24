38956c96cbb45cf5a9eb035d90f9918edef32d5f
Lucas Simidu <lucas.simidu@outlook.com>
2025-08-24 03:43:55 -0300

    feat: modules using examples

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 const { Person } = require('./person');
 
+//require('./modules/path');
+//require('./modules/fs');
+//require('./modules/http');
+
+require('./modules/express')
+
 const person = new Person('Helena');
 console.log(person.sayMyName());
