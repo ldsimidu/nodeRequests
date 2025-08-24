21cf98f4ba2b97dad801b1999d9fde8c6c0fd024
Lucas Simidu <lucas.simidu@outlook.com>
2025-08-24 04:36:32 -0300

    feat: get users w/UserModel

--- modules/express.js
+++ modules/express.js
@@ -2,29 +2,25 @@ const express = require('express');
 const UserModel = require('../src/models/user.model')
 
 const app = express();
-app.use(express.json())
+app.use(express.json()) // declarando que o express irá receber dados em json
 
 app.get('/home', (req, res) => {
     res.contentType("application/html");
     res.status(200).send('<h1>OPAAAAA</h1>');
 })
 
-app.get('/users', (req, res) => {
-    const users = [
-            {
-                name: 'Geralt de Rivia',
-                email: 'geraltrivia@gmail.com'
-            },
-            {
-                name: 'Yennefer de Vengerberg',
-                email: 'yennefervenger@gmail.com'
-            }
-        ];
-    
-    res.status(200).send(JSON.stringify(users))
+app.get('/users', async (req, res) => {
+    try {
+        const users = await UserModel.find({}); //pega todos os users
+
+        res.status(200).json(users);
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
 })
 
 app.post('/users', async (req, res) => {
+    // post no postman, url: http://localhost:8080/users
     try {
         const user = await UserModel.create(req.body)
         res.status(201).json(user)
